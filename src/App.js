97279c423ee5841531cb6fac6622c219de62b5e5
Suyash Lawand <suyash.lawand@gmail.com>
2021-12-15 15:03:03 +0530

    added under const.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Alert, AlertDescription, AlertIcon, AlertTitle, CloseButton, Link } from '@chakra-ui/react';
 import './App.css';
 import Landing from './components/landing/Landing';
 import Navbar from './components/Navbar';
@@ -5,6 +6,12 @@ import Navbar from './components/Navbar';
 function App() {
   return (
     <>
+      <Alert status='warning'>
+        <AlertIcon />
+        <AlertTitle mr={2}>Site under construction.</AlertTitle>
+        <AlertDescription>Site is under development.<Link href="mailto:admin@hiresuyash.com"> Click here</Link> to contact us.</AlertDescription>
+        <CloseButton position='absolute' right='8px' top='8px' />
+      </Alert>
       <Navbar />
       <Landing />
     </>
