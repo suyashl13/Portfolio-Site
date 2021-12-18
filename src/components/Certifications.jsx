c33fd24899373b6d2e0c0b6a4a8aa56ce25f5cc7
Suyash Lawand <suyash.lawand@gmail.com>
2021-12-18 12:20:53 +0530

    activated certificate links

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -18,42 +18,42 @@ export default function Certifications() {
     {
       title: "Node.js",
       icon: <FaNode fontSize="4rem" color="#718096" />,
-      link: "",
+      link: "https://courses.learncodeonline.in/learn/certificate/1490773-23129",
     },
     {
       title: "React.js",
       icon: <FaReact fontSize="4rem" color="#718096" />,
-      link: "",
+      link: "https://courses.learncodeonline.in/learn/certificate/1490773-71676",
     },
     {
       title: "Django",
       icon: <DiDjango fontSize="4rem" color="#718096" />,
-      link: "",
+      link: "https://courses.learncodeonline.in/learn/certificate/1490773-17379",
     },
     {
       title: "Flutter",
       icon: <RiFlutterFill fontSize="4rem" color="#718096" />,
-      link: "",
+      link: "https://courses.learncodeonline.in/learn/certificate/1490773-36624",
     },
     {
       title: "Android",
       icon: <SiAndroid fontSize="4rem" color="#718096" />,
-      link: "",
+      link: "https://courses.learncodeonline.in/learn/certificate/1490773-27376",
     },
     {
       title: "Java",
       icon: <FaJava fontSize="4rem" color="#718096" />,
-      link: "",
+      link: "https://udemy-certificate.s3.amazonaws.com/pdf/UC-8053b84c-4160-485f-ad87-1d1e4fb5c6d8.pdf",
     },
     {
       title: "Dart",
       icon: <SiDart fontSize="4rem" color="#718096" />,
-      link: "",
+      link: "https://courses.learncodeonline.in/learn/certificate/1490773-35916",
     },
     {
       title: "React & django Integration",
       icon: <FaCode fontSize="4rem" color="#718096" />,
-      link: "",
+      link: "https://courses.learncodeonline.in/learn/certificate/1490773-71676",
     },
   ];
 
@@ -91,8 +91,8 @@ export default function Certifications() {
                 }}
                 fontSize={{ base: "md", md: "lg", lg: "lg", sm: "md" }}
                 fontWeight={{
-                  base: "bold",
-                  sm: "bolder",
+                  base: "regular",
+                  sm: "regular",
                   md: "bolder",
                   lg: "bolder",
                 }}
@@ -128,9 +128,21 @@ export default function Certifications() {
   );
 }
 
-function CertificateBox({ title, icon }) {
+function CertificateBox({ title, icon, link }) {
   return (
-    <Box className="cert_box" w="100%" p="14px" border="solid #718096 2px">
+    <Box
+      onClick={() => {
+        window.open(link);
+      }}
+      _hover={{
+        boxShadow: "0px 0px 4px grey",
+      }}
+      borderRadius='2.8px'
+      className="cert_box"
+      w="100%"
+      p="14px"
+      border="solid #718096 2px"
+    >
       <Center
         className="txt"
         display="flex"
